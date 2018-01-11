40168a9f2df731e5d83fcf93f29352608b45d92f
Raphael Yancey <raphaelv.stud@gmail.com>
2018-01-11 18:23:52 +0100

    Update fish_greeting.fish

--- fish_greeting.fish
+++ fish_greeting.fish
@@ -1,10 +1,4 @@
-# Configure greeting with:
-#   -> set -g simple_ass_prompt_greeting MyGreeting
-
-set -g simple_ass_prompt_greeting (fortune | cowsay | lolcat)
-
 function fish_greeting
-  set -q simple_ass_prompt_greeting; or set -l simple_ass_prompt_greeting 'Where the hell is Science?!'
-
-  echo $simple_ass_prompt_greeting
+  fortune | cowsay | lolcat
+  echo -e \n
 end
